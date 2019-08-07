060be4a632a0f49b8845be0af7017387328206b2
Alex Privalsky <privalsky@mail.ru>
2019-08-07 23:46:43 +0300

    gg

---
+++ lecture02/src/main/java/ru/atom/geometry/Bar.java
@@ -0,0 +1,74 @@
+package ru.atom.geometry;
+
+public class Bar implements Collider{
+
+	private final Point topLeft;
+	private final int height;
+	private final int width;
+	
+	
+	public Bar(Point point1, Point point2) {
+		
+		topLeft = new Point(Math.min(point1.getX(), point2.getX()),Math.max(point1.getY(), point2.getY()));
+		width = Math.abs(point1.getX() - point2.getX());
+        height = Math.abs(point2.getY() - point2.getY());
+	}
+		
+	
+	
+	
+	@Override
+	public boolean isColliding(Collider other) {
+	
+		if(other instanceof Point) return contains((Point)other);
+		if(other instanceof Bar) return intersects((Bar)other);
+		
+		return false;
+	}
+
+	
+	private boolean intersects(Bar otherBar) {
+		
+	    Point thisCenter = getCenter(this);
+        Point otherCenter = getCenter(otherBar);
+        int xDist = Math.abs(thisCenter.getX() - otherCenter.getX());
+        int yDist = Math.abs(thisCenter.getY() - otherCenter.getY());
+        return xDist <= (width + otherBar.width) / 2 && yDist <= (height + otherBar.height) / 2;
+        
+		
+	}
+
+
+	private static Point getCenter(Bar bar) {
+        int x = bar.topLeft.getX() + bar.width / 2;
+        int y = bar.topLeft.getY() - bar.height / 2;
+        return new Point(x, y);
+    }
+
+	private boolean contains(Point point) {
+		 return point.getX() >= topLeft.getX()
+				&& point.getY() <= topLeft.getY()
+				&& point.getX() <= (topLeft.getX() + width)
+				&& point.getY() >= (topLeft.getY() - height);
+
+	}
+
+
+
+
+	@Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Bar)) {
+            return false;
+        }
+        Bar otherBar = (Bar) o;
+        return topLeft.equals(otherBar.topLeft) 
+        		&& width == otherBar.width 
+        		&& height == otherBar.height;
+    }
+	
+	
+}
--- lecture02/src/main/java/ru/atom/geometry/Point.java
+++ lecture02/src/main/java/ru/atom/geometry/Point.java
@@ -3,11 +3,27 @@ package ru.atom.geometry;
 /**
  * Template class for
  */
-public class Point /* super class and interfaces here if necessary */ {
+public class Point implements Collider /* super class and interfaces here if necessary */ {
     // fields
     // and methods
+private final int x;
+private final int y;
 
-    /**
+public Point(int x,int y) {
+this.x = x;
+this.y = y;
+}
+    public int getX() {
+	return x;
+}
+
+
+public int getY() {
+	return y;
+}
+
+
+	/**
      * @param o - other object to check equality with
      * @return true if two points are equal and not null.
      */
@@ -18,8 +34,16 @@ public class Point /* super class and interfaces here if necessary */ {
 
         // cast from Object to Point
         Point point = (Point) o;
-
-        // your code here
-        throw new UnsupportedOperationException();
+        if(x == point.x && y == point.y) return true;
+        return false;
     }
+	@Override
+	public boolean isColliding(Collider other) {
+		if(other instanceof Point) {
+			if(this.equals(other)) return true;
+		}
+		
+		if(other instanceof Bar) return other.isColliding(this);
+		return false;
+	}
 }
